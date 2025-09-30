1387650d3a76c1fc4c49e8353bcbb2ecb1b4d486
Anjali22-07 <anjalis2277@gmail.com>
2025-09-30 20:01:20 +0530

    "Handling Exceptions in mvc"

--- Springmvcsearch/src/main/java/SpringSearch/SearchController.java
+++ Springmvcsearch/src/main/java/SpringSearch/SearchController.java
@@ -1,8 +1,11 @@
 package SpringSearch;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
@@ -12,7 +15,8 @@ public class SearchController {
 	public String home() {
 		
 		System.out.print("This is home Page");
-		
+		String s=null;
+		System.out.print(s.length());
 		
 		return "home";
 	}
@@ -26,4 +30,14 @@ public class SearchController {
 		
 		return redirectView;
 	}
+	
+	//to handle Exception
+	
+	@ResponseStatus(value=HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(value=Exception.class)
+	public String exceptionHandler() {
+		
+		return "errors";
+	}
+	
 }
---
+++ Springmvcsearch/src/main/webapp/WEB-INF/View/errors.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@page isELIgnored="false" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<div class="container m-5">
+<div class="card">
+<div class="card-body m-5">
+
+<h1> OOPS!</h1>
+<h2>Something went wrong</h2>
+
+
+</div>
+
+</div>
+
+</div>
+
+
+
+
+
+</body>
+</html>
